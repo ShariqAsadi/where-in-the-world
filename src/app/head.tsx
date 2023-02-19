26de0b9fefd28d5e3e549e5d639b73d69a286172
Shariq Asadi <shariq.asadi@gmail.com>
2023-02-19 14:35:47 +0500

    Adds Tailwind and removes unnecessary styles

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Create Next App</title>
+      <title>Where in the world</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta
         name='description'
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import './globals.css';
 
+import { Nunito_Sans } from '@next/font/google';
+
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  weight: ['300', '600', '800'],
+});
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang='en'>
+    <html lang='en' className={nunitoSans.className}>
       <head />
       <body>{children}</body>
     </html>
