38e6bbb444294023618e637175525e775f317655
Shariq Asadi <shariq.asadi@gmail.com>
2023-02-19 13:46:52 +0500

    Updates Readme and seperates typing into an interface

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Where in the world
+
+## Overview
+
+### A nice overview to see details of all countries.
+
+<br />
+
+This app features:
+
+- See all countries from the API on the homepage.
+- Search for a country.
+- Filter countries by region.
+- Click on a country to see more detailed information on a separate page.
+- Click through to the border countries on the detail page to see their details information on a separate page.
+- Light and dark mode.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import './globals.css'
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='en'>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
