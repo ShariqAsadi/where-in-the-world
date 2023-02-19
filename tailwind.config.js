5d316c9f79b95f7ed3868ee5cc2c03c88a8bc928
Shariq Asadi <shariq.asadi@gmail.com>
2023-02-19 16:27:09 +0500

    Adds colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-elements': 'hsl(209, 23%, 22%)',
+        'dark-background': 'hsl(207, 26%, 17%)',
+        'light-text': 'hsl(200, 15%, 8%)',
+        'light-input': 'hsl(0, 0%, 52%)',
+        'light-background': 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
