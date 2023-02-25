20964e86c25f7825bf3a38970aa31f8f91a17dbf
Shariq Asadi <shariq.asadi@gmail.com>
2023-02-26 00:02:15 +0500

    Updates prettier config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 
-import { Nunito_Sans } from "next/font/google";
+import { Nunito_Sans } from 'next/font/google';
 import { BaseChildren } from '@/types/common';
 import Providers from './Providers';
 import Navbar from '@/components/Navbar';
