9c4ddc4f4773b324154b0b38c92056f88d39cc65
Shariq Asadi <shariq.asadi@gmail.com>
2023-03-08 09:37:52 +0500

    Simplifies Country card props and adds debounce

--- src/components/AllCountries.tsx
+++ src/components/AllCountries.tsx
@@ -1,6 +1,7 @@
 'use client';
+import debounce from 'lodash/debounce';
 import { Countries } from '@/types/common';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import CountryCard from './CountryCard';
 import Search from './Search';
 
@@ -12,31 +13,27 @@ export default function AllCountries(props: CountriesProps) {
   const { countries } = props;
   const [searchedCountries, setSearchedCountries] = useState(countries);
 
-  const handleSearch = (value: string) => {
-    const filteredCountries = countries.filter((country) =>
-      country.name.common.toLowerCase().includes(value)
-    );
-    setSearchedCountries(filteredCountries);
+  const debouncedSearch = useCallback(
+    debounce((query: string) => {
+      const filteredCountries = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(query.toLowerCase())
+      );
+      setSearchedCountries(filteredCountries);
+    }, 300),
+    []
+  );
+
+  const handleSearch = (query: string) => {
+    debouncedSearch(query);
   };
 
   return (
     <div className='sm:px-20 sm:pt-12 px-4 pt-6'>
       <Search handleSearch={handleSearch} />
       <section className='flex gap-[75px] flex-wrap mt-12 justify-center'>
-        {searchedCountries.map((country) => {
-          const { name, flags, capital, population, region } = country;
-          return (
-            <CountryCard
-              key={name.common}
-              image={flags.svg}
-              imageAlt={flags.alt}
-              name={name.common}
-              capital={capital[0]}
-              population={population}
-              region={region}
-            />
-          );
-        })}
+        {searchedCountries.map((country) => (
+          <CountryCard key={country.name.common} country={country} />
+        ))}
       </section>
     </div>
   );
--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,28 +1,24 @@
+import { Country } from '@/types/common';
 import Image from 'next/image';
 import CountryAttribute from './CountryAttribute';
 
 interface CountryCardProps {
-  image: string;
-  name: string;
-  population: number;
-  region: string;
-  capital: string;
-  imageAlt: string;
+  country: Country;
 }
 
 export default function CountryCard(props: CountryCardProps) {
-  const { image, name, population, region, capital, imageAlt } = props;
+  const { flags, name, population, region, capital } = props.country;
   return (
     <article className='flex flex-col rounded-md w-[264px] overflow-hidden'>
       <picture className='w-full h-[160px] relative'>
-        <Image fill alt={imageAlt} src={image} />
+        <Image fill alt={flags.alt} src={flags.svg} />
       </picture>
 
       <footer className='p-6'>
-        <h4 className='text-lg font-extrabold mb-4'>{name}</h4>
+        <h4 className='text-lg font-extrabold mb-4'>{name.common}</h4>
         <CountryAttribute name='Population' value={population} />
         <CountryAttribute name='Region' value={region} />
-        <CountryAttribute name='Capital' value={capital} />
+        <CountryAttribute name='Capital' value={capital[0]} />
       </footer>
     </article>
   );
